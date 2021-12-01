f95cdd61bd6f87077b6c9e2a90f9e3f5b4f240fa
zhangyifan <yifan2.zhang@aihuishou.com>
2021-12-01 18:33:39 +0800

    update

--- migration-example/src/main/java/priv/ethanzhang/migration/example/event/MyMigrationEventSubscriber.java
+++ migration-example/src/main/java/priv/ethanzhang/migration/example/event/MyMigrationEventSubscriber.java
@@ -2,6 +2,11 @@ package priv.ethanzhang.migration.example.event;
 
 import org.junit.Test;
 import priv.ethanzhang.migration.core.event.*;
+import priv.ethanzhang.migration.core.reader.EasyExcelMigrationReader;
+import priv.ethanzhang.migration.core.reader.MigrationReader;
+import priv.ethanzhang.migration.core.utils.GenericUtil;
+
+import java.io.File;
 
 public class MyMigrationEventSubscriber implements MigrationEventSubscriber<MigrationTaskLifecycleEvent> {
 
@@ -16,6 +21,13 @@ public class MyMigrationEventSubscriber implements MigrationEventSubscriber<Migr
         LocalMigrationEventDispatcher.INSTANCE.addSubsriber(subscriber);
         LocalMigrationEventDispatcher.INSTANCE.dispatch(new MigrationTaskFailedEvent(null, null, null));
         LocalMigrationEventDispatcher.INSTANCE.dispatch(new MigrationTaskWarnningEvent(null, null, null));
+
+        EasyExcelMigrationReader<Integer> reader1 = new EasyExcelMigrationReader<Integer>(new File("")) {};
+
+        EasyExcelMigrationReader<Integer> reader2 = new EasyExcelMigrationReader<>(new File(""));
+
+        System.out.println(GenericUtil.getInterfaceGenericType(reader1.getClass(), MigrationReader.class, 0));
+
     }
 
 }
